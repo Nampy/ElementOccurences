e26cc8866fd6d716b8a1590cf0ed4273c8d070e5
Nampy <at_ben@encs.concordia.ca>
2018-07-09 18:43:47 -0400

    Initial commit

---
+++ src/com/company/EnoughIsEnough.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class EnoughIsEnough {
+
+    public EnoughIsEnough(){
+
+    }
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+        // return an array
+        // example input: ([1,2,3,1,2,1,2,3],2) Output: [1,2,3,1,2,3]
+        // 
+        return null;
+    }
+}
