b4848f2491e7947d50adcf149cf3c29622cfc5d5
Nampy <at_ben@encs.concordia.ca>
2018-07-09 19:01:18 -0400

    final answer

--- src/com/company/EnoughIsEnough.java
+++ src/com/company/EnoughIsEnough.java
@@ -1,14 +1,28 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class EnoughIsEnough {
 
-    public EnoughIsEnough(){
+    private EnoughIsEnough(){
 
     }
     public static int[] deleteNth(int[] elements, int maxOccurrences) {
         // return an array
         // example input: ([1,2,3,1,2,1,2,3],2) Output: [1,2,3,1,2,3]
-        // 
-        return null;
+        // make sure numbers in the list only repeat n times, and return a list without changing the position any of the integers
+        ArrayList<Integer> temp_arrayList = new ArrayList<>();
+        for(int i = 0; i<elements.length; i++){
+            int frequency = Collections.frequency(temp_arrayList, elements[i]);
+            if(frequency< maxOccurrences){
+                temp_arrayList.add(elements[i]);
+            }
+        }
+        int[] arr = new int[temp_arrayList.size()];
+        for(int i = 0; i < temp_arrayList.size(); i++){
+            arr[i] = temp_arrayList.get(i);
+        }
+        return arr;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr = {20,37,20,21};
+        int[] ans = {};
+        ans = EnoughIsEnough.deleteNth(arr,1);
+        //testing the answer
+        System.out.println(ans[0]);
+        System.out.println(ans[1]);
+        System.out.println(ans[2]);
     }
 }
